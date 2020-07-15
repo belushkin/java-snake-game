fe42521db6f232abcaaaca9638e97dbd377c7664
Max Belushkin <maxb@onehourtranslation.com>
2020-07-15 18:10:31 +0300

    Added new implementation of dijkstra

--- src/com/snake/game/DijkstraAdopted.java
+++ src/com/snake/game/DijkstraAdopted.java
@@ -18,7 +18,7 @@ public class DijkstraAdopted {
     private static class Node {
         public int x;
         public int y;
-        public int value = 1;
+        public int value;
 
         public Node(int x, int y) {
             this.x = x;
@@ -49,21 +49,30 @@ public class DijkstraAdopted {
         public int hashCode() {
             return Objects.hash(x, y);
         }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    ", value=" + value +
+                    '}';
+        }
     }
 
     private static class SquareGrid {
 
         private int width;
         private int height;
-        private List<Node> weights = new ArrayList<>();
+        private Map<Node, Integer> weights = new HashMap<>();
 
         public SquareGrid(int width, int height) {
             this.width = width;
             this.height = height;
         }
 
-        public void addWeight(Node node) {
-            weights.add(node);
+        public void addWeight(Node node, int weight) {
+            weights.put(node, weight);
         }
 
         private boolean inBounds(int x, int y) {
@@ -83,6 +92,10 @@ public class DijkstraAdopted {
                     filter(node -> inBounds(node.x, node.y)).
                     collect(Collectors.toList());
         }
+
+        private int cost(Node from, Node to) {
+            return weights.getOrDefault(to, 1);
+        }
     }
 
     public boolean search(SquareGrid grid, Node start, Node goal) {
@@ -95,39 +108,65 @@ public class DijkstraAdopted {
 
         Map<Node, Integer> A = new HashMap<>(); // cost so far
         Map<Node, Node> B = new HashMap<>(); // came from
+
+        // initial cost of the source is 0
+        A.put(start, 0);
+
+        // We did not come to the source from anywhere
+        B.put(start, null);
+
+        while (!frontier.isEmpty()) {
+            Node current = frontier.poll();
+
+            if (current.equals(goal)) {
+                break;
+            }
+
+            for (Node next: grid.neighbors(current.x, current.y)) {
+                int newCost = A.get(current) + grid.cost(current, next);
+                if (!A.containsKey(next) || newCost < A.get(next)) {
+                    A.put(next, newCost);
+                    frontier.add(next.setValue(newCost));
+                    B.put(next, current);
+                }
+            }
+
+        }
+        System.out.println(A);
+
         return true;
     }
 
     public static void main(String[] args) {
         DijkstraAdopted dijkstra = new DijkstraAdopted();
         SquareGrid grid = new SquareGrid(10, 10);
-        grid.addWeight(new Node(3,4, 5));
-        grid.addWeight(new Node(3,5, 5));
-        grid.addWeight(new Node(4,1, 5));
-        grid.addWeight(new Node(4,2, 5));
-        grid.addWeight(new Node(4,3, 5));
-        grid.addWeight(new Node(4,4, 5));
-        grid.addWeight(new Node(4,5, 5));
-        grid.addWeight(new Node(4,6, 5));
-        grid.addWeight(new Node(4,7, 5));
-        grid.addWeight(new Node(4,8, 5));
-        grid.addWeight(new Node(5,1, 5));
-        grid.addWeight(new Node(5,2, 5));
-        grid.addWeight(new Node(5,3, 5));
-        grid.addWeight(new Node(5,4, 5));
-        grid.addWeight(new Node(5,5, 5));
-        grid.addWeight(new Node(5,6, 5));
-        grid.addWeight(new Node(5,7, 5));
-        grid.addWeight(new Node(5,8, 5));
-        grid.addWeight(new Node(6,2, 5));
-        grid.addWeight(new Node(6,3, 5));
-        grid.addWeight(new Node(6,4, 5));
-        grid.addWeight(new Node(6,5, 5));
-        grid.addWeight(new Node(6,6, 5));
-        grid.addWeight(new Node(6,7, 5));
-        grid.addWeight(new Node(7,3, 5));
-        grid.addWeight(new Node(7,4, 5));
-        grid.addWeight(new Node(7,5, 5));
+        grid.addWeight(new Node(3,4), 5);
+        grid.addWeight(new Node(3,5), 5);
+        grid.addWeight(new Node(4,1), 5);
+        grid.addWeight(new Node(4,2), 5);
+        grid.addWeight(new Node(4,3), 5);
+        grid.addWeight(new Node(4,4), 5);
+        grid.addWeight(new Node(4,5), 5);
+        grid.addWeight(new Node(4,6), 5);
+        grid.addWeight(new Node(4,7), 5);
+        grid.addWeight(new Node(4,8), 5);
+        grid.addWeight(new Node(5,1), 5);
+        grid.addWeight(new Node(5,2), 5);
+        grid.addWeight(new Node(5,3), 5);
+        grid.addWeight(new Node(5,4), 5);
+        grid.addWeight(new Node(5,5), 5);
+        grid.addWeight(new Node(5,6), 5);
+        grid.addWeight(new Node(5,7), 5);
+        grid.addWeight(new Node(5,8), 5);
+        grid.addWeight(new Node(6,2), 5);
+        grid.addWeight(new Node(6,3), 5);
+        grid.addWeight(new Node(6,4), 5);
+        grid.addWeight(new Node(6,5), 5);
+        grid.addWeight(new Node(6,6), 5);
+        grid.addWeight(new Node(6,7), 5);
+        grid.addWeight(new Node(7,3), 5);
+        grid.addWeight(new Node(7,4), 5);
+        grid.addWeight(new Node(7,5), 5);
 
         boolean res = dijkstra.search(
                 grid,
